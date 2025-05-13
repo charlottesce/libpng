cb70e2d02f53ac26d130230331b9179bd217d112
Léonie Louise Dezempte <dwii@MacbookproDWll.local>
2025-05-13 16:19:33 +0200

    new fuzzer

--- contrib/oss-fuzz/png_get_metadata_fuzzer.cc
+++ contrib/oss-fuzz/png_get_metadata_fuzzer.cc
@@ -52,14 +52,9 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
   for (png_uint_32 y = 0; y < height; y++) {
     row_pointers[y] = (png_bytep)malloc(png_get_rowbytes(png_ptr, info_ptr));
   }
-  png_read_image(png_ptr, row_pointers);
-  for (png_uint_32 y = 0; y < height; y++) {
-    free(row_pointers[y]);
-  }
-  free(row_pointers);
+  // Exploitation réelle des métadonnées
 
-
-  // 🔍 Exploitation réelle des métadonnées
+  //time
   png_timep mod_time_ptr = NULL;
   if (png_get_tIME(png_ptr, end_info_ptr, &mod_time_ptr)) {
       if (mod_time_ptr && mod_time_ptr->year > 2020) {
@@ -67,6 +62,7 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
       }
   }
 
+  //text
   png_textp text_ptr;
   int num_text;
   if (png_get_text(png_ptr, info_ptr, &text_ptr, &num_text)) {
@@ -77,6 +73,7 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
     }
   }
 
+  //gama 
   double gamma;
   if (png_get_gAMA(png_ptr, info_ptr, &gamma)) {
     if (gamma > 0.5) {
@@ -84,6 +81,7 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
     }
   }
 
+  //phys
   png_uint_32 res_x, res_y;
   int unit_type;
   if (png_get_pHYs(png_ptr, info_ptr, &res_x, &res_y, &unit_type)) {
@@ -92,7 +90,73 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
       }
   }
 
-  png_uint_32 res_x, res_y;
+  //chrm 
+  double w_x, w_y, r_x, r_y, g_x, g_y, b_x, b_y;
+
+  if (png_get_cHRM(png_ptr, info_ptr,&w_x, &w_y,
+                  &r_x, &r_y,
+                  &g_x, &g_y,
+                  &b_x, &b_y)) {
+      volatile double sum = w_x + w_y + r_x + g_y;
+  }
+
+  //chrm xyz
+  double red_X, red_Y, red_Z, green_X, green_Y, green_Z, blue_X, blue_Y, blue_Z;
+  png_get_cHRM_XYZ(png_ptr, info_ptr, &red_X, &red_Y, &red_Z, &green_X, &green_Y, &green_Z, &blue_X, &blue_Y, &blue_Z);
+
+  //chrm fixed
+  png_fixed_point white_x, white_y, red_x, red_y, green_x, green_y, blue_x, blue_y;
+  if (png_get_cHRM_fixed(png_ptr, info_ptr, &white_x, &white_y, &red_x, &red_y, &green_x, &green_y, &blue_x, &blue_y)) {
+    if (white_x > 10000) {
+      volatile int trigger = white_x + white_y + red_x; ;
+    }
+  }
+
+  // chrm xyz fixed
+  png_fixed_point ired_X, ired_Y, ired_Z, igreen_X, igreen_Y, igreen_Z, iblue_X, iblue_Y, iblue_Z;
+  png_get_cHRM_XYZ_fixed(png_ptr, info_ptr, &ired_X, &ired_Y, &ired_Z, &igreen_X, &igreen_Y, &igreen_Z, &iblue_X, &iblue_Y, &iblue_Z);
+
+
+  // sRGB
+  int srgb_intent;
+  if (png_get_sRGB(png_ptr, info_ptr, &srgb_intent)) {
+      volatile int dummy = srgb_intent;
+  }
+
+  //sBIT
+  png_color_8p sig_bits = NULL;
+
+  if (png_get_sBIT(png_ptr, info_ptr, &sig_bits)) {
+      // Use the significant bits to trigger instrumentation
+      volatile int r = sig_bits->red;
+      volatile int g = sig_bits->green;
+      volatile int b = sig_bits->blue;
+  }
+
+  //bKGD
+  png_color_16p background;
+
+  if (png_get_bKGD(png_ptr, info_ptr, &background)) {
+      // Access background color components (RGB version)
+      volatile int r = background->red;
+      volatile int g = background->green;
+      volatile int b = background->blue;
+  }
+
+  //bKGD
+  png_colorp palette = NULL;
+  int num_palette = 0;
+
+  if (png_get_PLTE(png_ptr, info_ptr, &palette, &num_palette)) {
+      if (num_palette > 0 && palette != NULL) {
+          // Access the first palette color (red, green, blue)
+          volatile int r = palette[0].red;
+          volatile int g = palette[0].green;
+          volatile int b = palette[0].blue;
+      }
+  }
+
+  /*png_uint_32 res_x, res_y;
   int unit_type;
   if (png_get_pHYs_dpi(png_ptr, info_ptr, &res_x, &res_y, &unit_type)) {
     if (res_x > 300 || res_y > 300) {
@@ -100,25 +164,6 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
     }
   }
 
-  png_colorp palette = NULL;
-  int num_palette = 0;
-
-  if (png_get_PLTE(png_ptr, info_ptr, &palette, &num_palette)) {
-      if (num_palette > 0 && palette != NULL) {
-          // Just access the first color to ensure memory is read
-          volatile int r = palette[0].red;
-          volatile int g = palette[0].green;
-          volatile int b = palette[0].blue;
-      }
-  }
-
-
-  png_fixed_point white_x, white_y, red_x, red_y, green_x, green_y, blue_x, blue_y;
-  if (png_get_cHRM_fixed(png_ptr, info_ptr, &white_x, &white_y, &red_x, &red_y, &green_x, &green_y, &blue_x, &blue_y)) {
-    if (white_x > 10000) {
-      volatile int trigger = white_x + white_y + red_x; ;
-    }
-  }
   int channels = png_get_channels(png_ptr, info_ptr);
   png_byte color_type2 = png_get_color_type(png_ptr, info_ptr);
 
@@ -132,37 +177,6 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
           volatile int d = gamma_fp;
       }
   }
-
-  double wx, wy, rx, ry, gx, gy, bx, by;
-  png_get_cHRM(png_ptr, info_ptr, &wx, &wy, &rx, &ry, &gx, &gy, &bx, &by);
-
-  png_fixed_point fwx, fwy, frx, fry, fgx, fgy, fbx, fby;
-  png_get_cHRM_fixed(png_ptr, info_ptr, &fwx, &fwy, &frx, &fry, &fgx, &fgy, &fbx, &fby);
-
-  double red_X, red_Y, red_Z, green_X, green_Y, green_Z, blue_X, blue_Y, blue_Z;
-  png_get_cHRM_XYZ(png_ptr, info_ptr, &red_X, &red_Y, &red_Z, &green_X, &green_Y, &green_Z, &blue_X, &blue_Y, &blue_Z);
-
-  png_fixed_point ired_X, ired_Y, ired_Z, igreen_X, igreen_Y, igreen_Z, iblue_X, iblue_Y, iblue_Z;
-  png_get_cHRM_XYZ_fixed(png_ptr, info_ptr, &ired_X, &ired_Y, &ired_Z, &igreen_X, &igreen_Y, &igreen_Z, &iblue_X, &iblue_Y, &iblue_Z);
-
-    // sRGB
-  int srgb_intent;
-  if (png_get_sRGB(png_ptr, info_ptr, &srgb_intent)) {
-      volatile int dummy = srgb_intent;
-  }
-
-  // sBIT
-  png_color_8p sig_bits;
-  if (png_get_sBIT(png_ptr, info_ptr, &sig_bits)) {
-      volatile int d = sig_bits->red + sig_bits->green + sig_bits->blue;
-  }
-
-  // bKGD
-  png_color_16p background;
-  if (png_get_bKGD(png_ptr, info_ptr, &background)) {
-      volatile int bg = background->red + background->green + background->blue;
-  }
-
   // hIST
   png_uint_16p hist = NULL;
   int num_palette = 0;
@@ -170,9 +184,13 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
   png_get_PLTE(png_ptr, info_ptr, &palette, &num_palette); // Ensure palette is read
   if (num_palette > 0 && png_get_hIST(png_ptr, info_ptr, &hist)) {
       volatile int h = hist[0];
+  }*/
+
+  png_read_image(png_ptr, row_pointers);
+  for (png_uint_32 y = 0; y < height; y++) {
+    free(row_pointers[y]);
   }
-
-
+  free(row_pointers);
 
 
   png_destroy_read_struct(&png_ptr, &info_ptr, &end_info_ptr);
